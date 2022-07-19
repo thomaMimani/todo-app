2d27c7d0ddf6f13bdcd99b3cb640fa36a31576da
Thoma <hmimani@outlook.com>
2022-07-19 03:32:53 +0200

    no message

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ let titleEl = document.querySelector(`.title-list`)
 let descriptionEl = document.querySelector(`.description-list`)
 
 
-let todo = []
+// let todo = []
+// function add(){
+
+//     if(titleVal==''&& descriptionVal=='' ){
+//         return
+//     }else{
+        
+        
+//         todo.push([titleVal.value, descriptionVal.value])
+//         titleEl.innerHTML=todo[0]
+//         descriptionEl=todo[1]
+//         console.log(titleVal.value, descriptionVal.value)
+//     }
+// }
+
+// for (i of todo){
+//     titleEl.innerHTML=i[0]
+//     descriptionEl=i[1]
+//     console.log(i[0],i[1])
+// }
+
+let todo = {
+    constructor
+}
+
 function add(){
-    todo.push([titleVal.value, descriptionVal.value])
-    titleEl.innerHTML=todo[0]
-    descriptionEl=todo[1]
+    todo.set(titleVal.value, descriptionVal.value)
+    console.log(todo, ...todo.keys(), ...todo.values())
+    console.log(todoKeys);
 }
+let todoKeys = [...todo.keys()]
 
-for (i of todo){
-    titleEl.innerHTML=i[0]
-    descriptionEl=i[1]
-    console.log(i[0],i[1])
-}
-
+// titleEl.textContent=
+// descriptionEl.textContent=
